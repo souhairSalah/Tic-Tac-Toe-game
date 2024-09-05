059fa541299f6aebb64f49b635c6d9cdd726b08d
souhair salah <sou6sal@gmail.com>
2024-09-05 17:42:40 +0300

    edit in design

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -27,9 +27,12 @@ const Board = () => {
     status = "the winner is : "+ (winner);
   }
   else{
-    status = 'Next player:' +(xIsNext ? "X" : "O");
+    status = 'Next player : ' +(xIsNext ? "X" : "O");
   }
 
+  function resetTheGame(){
+    setSquares(Array(9).fill(null));
+  }
 
   return (
   <div className='grid gap-0'>
@@ -48,7 +51,13 @@ const Board = () => {
       <Square squareValue={squares[7]} onSquareClick={() => handleClick(7)}/>  
       <Square squareValue={squares[8]} onSquareClick={() => handleClick(8)}/> 
     </div>
-    <div>{status}</div>
+    <div className='m-auto mt-4 text-blue-500 font-bold text-2xl'>{status}</div>
+    <button 
+      onClick={resetTheGame} 
+      className='m-auto mt-4  font-medium text-xl text-white bg-[#60a5fa] border-blue-400 border-2 px-3 rounded-sm'
+    >
+      rest the game
+    </button>
   </div>
   )
 }
@@ -71,6 +80,13 @@ function calculatWinner(index:string[]){
     if(index[a]&&index[a]===index[b]&&index[a]===index[c]){
       return index[a];
     }
+    // work but not the best way
+    // for (let j=0 ; j<lines[i].length ; j++){
+    //   if (index[lines[i][0]]&&index[lines[i][0]]===index[lines[i][1]]&&index[lines[i][0]]===index[lines[i][2]]) {
+    //     return index[lines[i][0]];
+    //   }
+    // }
   }
   return null;
 }
+
--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -8,15 +8,12 @@ interface Props {
 
 type player = "X"|"O"|null|boolean ;
 
-const Square = ({squareValue, onSquareClick}:Props) => {
-  // const [squareValue, setSquareValue] = useState<player>(null); 
-
-  // function handleClick (){
-  //   setSquareValue("X");
-  // }      
+const Square = ({squareValue, onSquareClick}:Props) => {     
   return (
     <button 
-      className="cursor-pointer w-32 border-2 border-black h-32"
+      className={`cursor-pointer w-32 border-2 border-neutral-500 h-32 flex items-center justify-center text-4xl font-bold
+         ${squareValue=="X"?"text-blue-500":"text-red-500" }`
+      }
       onClick={onSquareClick}
     >
         {squareValue}
